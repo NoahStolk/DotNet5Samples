2f4a0d2b25c45cf9bdcaf39db94660503a6fcc88
Noah Stolk <noah.stolk1997@gmail.com>
2020-07-13 21:14:58 +0200

    Add array example to NewExpressionsSample

--- DotNet5Samples/TargetTyping/NewExpressions/NewExpressionsSample.cs
+++ DotNet5Samples/TargetTyping/NewExpressions/NewExpressionsSample.cs
@@ -9,12 +9,38 @@ namespace DotNet5Samples.TargetTyping.NewExpressions
 		{
 			Coordinate coord = new Coordinate(1, 2, 3);
 			Console.WriteLine(coord);
+
+			Console.WriteLine();
+
+			Coordinate[] coords = new Coordinate[]
+			{
+				new Coordinate(1, 4, 4),
+				new Coordinate(4, 5, 4),
+				new Coordinate(5, 4, 5),
+				new Coordinate(4, 2, 5),
+				new Coordinate(4, 5, 5),
+			};
+			foreach (Coordinate c in coords)
+				Console.WriteLine(c);
 		}
 
 		public void RunCSharp9()
 		{
 			Coordinate coord = new(1, 2, 3);
 			Console.WriteLine(coord);
+
+			Console.WriteLine();
+
+			Coordinate[] coords = new Coordinate[]
+			{
+				new(1, 4, 4),
+				new(4, 5, 4),
+				new(5, 4, 5),
+				new(4, 2, 5),
+				new(4, 5, 5),
+			};
+			foreach (Coordinate c in coords)
+				Console.WriteLine(c);
 		}
 	}
 }
