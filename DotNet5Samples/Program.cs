7a460630697ffb0573a5c3747640b5b3829677ea
Noah Stolk <noah.stolk1997@gmail.com>
2020-07-14 16:20:51 +0200

    Use top-level program

--- DotNet5Samples/Program.cs
+++ DotNet5Samples/Program.cs
@@ -1,65 +1,59 @@
-﻿using DotNet5Samples.PatternMatching.LogicalPatterns;
+﻿#pragma warning disable IDE0051 // Remove unused private members
+using DotNet5Samples;
+using DotNet5Samples.PatternMatching.LogicalPatterns;
 using DotNet5Samples.PatternMatching.RelationalPatterns;
 using DotNet5Samples.SourceGenerators;
 using DotNet5Samples.TargetTyping.NewExpressions;
 using DotNet5Samples.TargetTyping.NullCoalescenceExpressions;
 using System;
 
-namespace DotNet5Samples
+Console.WindowWidth = 140;
+Console.WindowHeight = 40;
+Console.ForegroundColor = ConsoleColor.White;
+
+for (; ; )
 {
-	public static class Program
+	ConsoleKey input = Console.ReadKey().Key;
+	Action action = input switch
 	{
-		public static void Main()
-		{
-			Console.WindowWidth = 140;
-			Console.WindowHeight = 40;
-			Console.ForegroundColor = ConsoleColor.White;
+		ConsoleKey.D0 or ConsoleKey.NumPad0 => () => RunSample<NewExpressionsSample>(),
+		ConsoleKey.D1 or ConsoleKey.NumPad1 => () => RunSample<NullCoalescenceExpressionsSample>(),
+		ConsoleKey.D2 or ConsoleKey.NumPad2 => () => RunSample<RelationalPatternsSample>(),
+		ConsoleKey.D3 or ConsoleKey.NumPad3 => () => RunSample<LogicalPatternsAndSample>(),
+		ConsoleKey.D4 or ConsoleKey.NumPad4 => () => RunSample<LogicalPatternsOrSample>(),
+		ConsoleKey.D5 or ConsoleKey.NumPad5 => () => RunSample<LogicalPatternsSimpleTypeSample>(),
+		ConsoleKey.D6 or ConsoleKey.NumPad6 => () => RunSample<LogicalPatternsNotSample>(),
+		ConsoleKey.D7 or ConsoleKey.NumPad7 => () => RunSample<SyntaxTreesGeneratorSample>(),
+		ConsoleKey.E => () => Environment.Exit(0),
+		_ => () => Console.WriteLine($"No action implemented for {nameof(ConsoleKey)} '{input}'."),
+	};
+	Console.BackgroundColor = ConsoleColor.Black;
+	Console.Clear();
+	action();
+}
 
-			for (; ; )
-			{
-				ConsoleKey input = Console.ReadKey().Key;
-				Action action = input switch
-				{
-					ConsoleKey.D0 or ConsoleKey.NumPad0 => () => RunSample<NewExpressionsSample>(),
-					ConsoleKey.D1 or ConsoleKey.NumPad1 => () => RunSample<NullCoalescenceExpressionsSample>(),
-					ConsoleKey.D2 or ConsoleKey.NumPad2 => () => RunSample<RelationalPatternsSample>(),
-					ConsoleKey.D3 or ConsoleKey.NumPad3 => () => RunSample<LogicalPatternsAndSample>(),
-					ConsoleKey.D4 or ConsoleKey.NumPad4 => () => RunSample<LogicalPatternsOrSample>(),
-					ConsoleKey.D5 or ConsoleKey.NumPad5 => () => RunSample<LogicalPatternsSimpleTypeSample>(),
-					ConsoleKey.D6 or ConsoleKey.NumPad6 => () => RunSample<LogicalPatternsNotSample>(),
-					ConsoleKey.D7 or ConsoleKey.NumPad7 => () => RunSample<SyntaxTreesGeneratorSample>(),
-					ConsoleKey.E => () => Environment.Exit(0),
-					_ => () => Console.WriteLine($"No action implemented for {nameof(ConsoleKey)} '{input}'."),
-				};
-				Console.BackgroundColor = ConsoleColor.Black;
-				Console.Clear();
-				action();
-			}
-		}
+static void RunSample<TSample>()
+	where TSample : ISample, new()
+{
+	TSample sample = new();
 
-		private static void RunSample<TSample>()
-			where TSample : ISample, new()
-		{
-			TSample sample = new();
+	RunSampleLanguageVersion(() => sample.RunCSharp8(), $"{sample.GetType().Name} (C# 8.0)", ConsoleColor.DarkBlue);
+	Console.WriteLine();
+	RunSampleLanguageVersion(() => sample.RunCSharp9(), $"{sample.GetType().Name} (C# 9.0)", ConsoleColor.DarkMagenta);
+}
 
-			RunSampleLanguageVersion(() => sample.RunCSharp8(), $"{sample.GetType().Name} (C# 8.0)", ConsoleColor.DarkBlue);
-			Console.WriteLine();
-			RunSampleLanguageVersion(() => sample.RunCSharp9(), $"{sample.GetType().Name} (C# 9.0)", ConsoleColor.DarkMagenta);
-		}
-
-		private static void RunSampleLanguageVersion(Action languageMethod, string displayName, ConsoleColor backgroundColor)
-		{
-			Console.BackgroundColor = backgroundColor;
-			Console.WriteLine($"{displayName}:");
-			try
-			{
-				languageMethod();
-			}
-			catch (Exception ex)
-			{
-				Console.BackgroundColor = ConsoleColor.DarkRed;
-				Console.WriteLine($"Failed: {ex.Message}");
-			}
-		}
+static void RunSampleLanguageVersion(Action languageMethod, string displayName, ConsoleColor backgroundColor)
+{
+	Console.BackgroundColor = backgroundColor;
+	Console.WriteLine($"{displayName}:");
+	try
+	{
+		languageMethod();
+	}
+	catch (Exception ex)
+	{
+		Console.BackgroundColor = ConsoleColor.DarkRed;
+		Console.WriteLine($"Failed: {ex.Message}");
 	}
 }
+#pragma warning restore IDE0051 // Remove unused private members
