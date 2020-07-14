24c6b62bf35859f00e793195eaf4f81ffc77e6ac
Noah Stolk <noah.stolk1997@gmail.com>
2020-07-14 16:23:26 +0200

    Change input keys

--- DotNet5Samples/Program.cs
+++ DotNet5Samples/Program.cs
@@ -16,15 +16,15 @@ for (; ; )
 	ConsoleKey input = Console.ReadKey().Key;
 	Action action = input switch
 	{
-		ConsoleKey.D0 or ConsoleKey.NumPad0 => () => RunSample<NewExpressionsSample>(),
-		ConsoleKey.D1 or ConsoleKey.NumPad1 => () => RunSample<NullCoalescenceExpressionsSample>(),
-		ConsoleKey.D2 or ConsoleKey.NumPad2 => () => RunSample<RelationalPatternsSample>(),
-		ConsoleKey.D3 or ConsoleKey.NumPad3 => () => RunSample<LogicalPatternsAndSample>(),
-		ConsoleKey.D4 or ConsoleKey.NumPad4 => () => RunSample<LogicalPatternsOrSample>(),
-		ConsoleKey.D5 or ConsoleKey.NumPad5 => () => RunSample<LogicalPatternsSimpleTypeSample>(),
+		ConsoleKey.D1 or ConsoleKey.NumPad1 => () => RunSample<NewExpressionsSample>(),
+		ConsoleKey.D2 or ConsoleKey.NumPad2 => () => RunSample<NullCoalescenceExpressionsSample>(),
+		ConsoleKey.D3 or ConsoleKey.NumPad3 => () => RunSample<RelationalPatternsSample>(),
+		ConsoleKey.D4 or ConsoleKey.NumPad4 => () => RunSample<LogicalPatternsAndSample>(),
+		ConsoleKey.D5 or ConsoleKey.NumPad5 => () => RunSample<LogicalPatternsOrSample>(),
 		ConsoleKey.D6 or ConsoleKey.NumPad6 => () => RunSample<LogicalPatternsNotSample>(),
-		ConsoleKey.D7 or ConsoleKey.NumPad7 => () => RunSample<SyntaxTreesGeneratorSample>(),
-		ConsoleKey.E => () => Environment.Exit(0),
+		ConsoleKey.D7 or ConsoleKey.NumPad7 => () => RunSample<LogicalPatternsSimpleTypeSample>(),
+		ConsoleKey.D8 or ConsoleKey.NumPad8 => () => RunSample<SyntaxTreesGeneratorSample>(),
+		ConsoleKey.Escape => () => Environment.Exit(0),
 		_ => () => Console.WriteLine($"No action implemented for {nameof(ConsoleKey)} '{input}'."),
 	};
 	Console.BackgroundColor = ConsoleColor.Black;
